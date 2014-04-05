49862e9c4adc43e1f2558fab83f66247d24074a3
cjustice <connorjustice@gmail.com>
2014-04-05 14:00:28 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Weshare::Application.routes.draw do
-  get "users/new"
+  resources :users
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
