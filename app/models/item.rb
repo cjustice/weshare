46f82c421d00b9aca6ba7ef57db1b7f8f8a501e5
Erik Islo <eislo@wesleyan.edu>
2014-04-07 02:01:53 -0400

    add validation on length of item description

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order('created_at DESC') }
+
+  validates :content, presence: true, length: { maximum: 400 }
   validates :user_id, presence: true
 end
