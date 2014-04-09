6b87e8dfdbd718b0d04d3fb78bb0db2fc6b340fe
cjustice <connorjustice@gmail.com>
2014-04-09 12:59:14 -0400

    Fix user page crashing if map is nil

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,6 @@ class UsersController < ApplicationController
 
   def index
     @users = User.paginate(page: params[:page])
-    @hash2 = Gmaps4rails.build_markers(@users) do |user, marker|
-      marker.lat user.latitude
-      marker.lng user.longitude
-    end
   end
 
   def show
