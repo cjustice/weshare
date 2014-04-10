cf96d9139be6637002ee03c634956e7f188bfc66
Erik Islo <eislo@wesleyan.edu>
2014-04-09 21:19:50 -0400

    add items view to profile page with minimal formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @items = @user.items #pagination code removed bc error– .paginate(page: params[:page])
   end
 
   def new
@@ -11,7 +12,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the WeShare!"
+      flash[:success] = "Welcome to WeShare!"
       redirect_to @user
     else
       render 'new'
@@ -24,4 +25,5 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
 end
